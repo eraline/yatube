54f12c5ab798e72ccbda6923de97f40f9e524b3c
Konstantin Kintushev <k.kintushev@gmail.com>
2021-10-29 00:29:56 +0300

    Fixed selffollowing

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -139,6 +139,8 @@ def follow_index(request):
 
 @login_required
 def profile_follow(request, username):
+    if request.user.username == username:
+        return redirect('posts:profile', username=username)
     author = get_object_or_404(User, username=username)
     Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', username=username)
