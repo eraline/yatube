1811a5d4a16dc765f01b4aedd12ace168ca1cced
Konstantin Kintushev <kkintushev@gmail.com>
2022-05-04 14:56:31 +0000

    changed statifcilres dirs

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = [
     'testserver',
     'www.eraline.pythonanywhere.com',
     'eraline.pythonanywhere.com',
+    '51.250.18.168',
+    'kkintushev.sytes.net',
 ]
 
 INTERNAL_IPS = [
@@ -134,7 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = '/static/'
 
